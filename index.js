1d9bb4c1cebf2738ef49cb49726c53b300ae27a8
Zeh Fernando <zeh@zehfernando.com>
2017-01-06 01:09:38 -0500

    Cleaner function interface
    No module exposition, no need to read files through FS, better option parameters

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
-const dasm = require("./lib/dasm.js");
+var dasm = require("./lib/dasm.js");
+var FILENAME_IN = "file.a";
+var FILENAME_OUT = "file.out";
+var FILENAME_LIST = "file.lst";
+var FILENAME_SYMBOLS = "file.sym";
 
-function print(s) {
-	// console.log("[DASM] ", s);
-}
-function printErr(s) {
-	// console.error("[DASM] ", s);
+function logLine(s) {
+	log.push(s);
 }
 
-function preRun() {
-	// console.log("[TEST] [PRE RUN]");
+function logErrorLine(s) {
+	log.push("[ERROR] " + s);
 }
 
 var options = {
 	noInitialRun: true,
-	print: print,
-	printErr: printErr,
-	preRun: preRun,
-	//arguments: ["_test.asm"],
-	//preInit func
+	print: logLine,
+	printErr: logErrorLine,
+	// Also available: preInit, preRun
 };
 
 var Module = dasm.DASM(options);
 var FS = Module ? Module['FS'] : undefined;
+var log = [];
+var logErrors = [];
 
-module.exports = function() {
+module.exports = function(src, options) {
+	options = options || {};
+
+	// Prepare vars
+	log = [];
+
+	// Prepare source
 	var src = arguments[0];
-	var filename = "in.a";
-	FS.writeFile(filename, src);
-	var args = [filename].concat(Array.prototype.slice.call(arguments).slice(1));
-	Module.callMain(args);
-	return Module;
+	FS.writeFile(FILENAME_IN, src);
+
+	// Prepare argument list
+	var args = [];
+	var isQuick = options.hasOwnProperty("quick") && options.quick;
+	args.push("-o" + FILENAME_OUT);
+	if (options.hasOwnProperty("format")) {
+		args.push("-f" + options.format);
+	} else {
+		args.push("-f3");
+	}
+	if (!isQuick) {
+		args.push("-L" + FILENAME_LIST);
+		args.push("-s" + FILENAME_SYMBOLS);
+	}
+
+	// Finally, call it
+	Module.callMain([FILENAME_IN].concat(args));
+	return {
+		data: Module.FS.readFile(FILENAME_OUT),
+		output: log.concat(),
+		list: isQuick ? undefined : Module.FS.readFile(FILENAME_LIST, { encoding: "utf8" }),
+		symbols: isQuick ? undefined : Module.FS.readFile(FILENAME_SYMBOLS, { encoding: "utf8" }),
+	};
 }
