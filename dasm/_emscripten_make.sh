17eadb4cbb81bfc0307c69e8c51e0acf99d2ae69
Zeh Fernando <zeh@zehfernando.com>
2017-01-06 22:03:59 -0500

    Updated emcc compilation script
    The new script uses code optimization and the --pre-js and --post-sjs switches for automatic injection of module wrapping code.

--- dasm/_emscripten_make.sh
+++ dasm/_emscripten_make.sh
@@ -1,6 +1,64 @@
+echo
+echo Compiling dasm with emscripten
+echo This requires the emsdk installed in /usr/bin/emsdk
+echo
+
+cd src
+source /usr/bin/emsdk/emsdk_env.sh
+emcc --version
+
+echo
+echo ============
+echo REMOVING OLD
+echo ============
+echo
+
 rm dasm.js
 rm src/dasm.bc
+
+echo
+echo ========
+echo CLEANING
+echo ========
+echo
+
 make clean
+
+echo
+echo =========
+echo COMPILING
+echo =========
+echo
+
 emmake make
+
+echo
+echo ==========
+echo GENERATING
+echo ==========
+echo
+
 mv src/dasm src/dasm.bc
-emcc src/dasm.bc -s FORCE_FILESYSTEM=1 -o dasm.js
+emcc -O3 --memory-init-file 0 -g1 src/dasm.bc -s FORCE_FILESYSTEM=1 --pre-js ../prejs.txt --post-js ../postjs.txt -o dasm.js
+mv dasm.js ..
+
+echo
+echo =================
+echo CLEANING UP AGAIN
+echo =================
+echo
+
+make clean
+rm src/dasm.bc
+
+echo
+echo ====
+echo DONE
+echo ====
+echo
+
+echo
+echo Library dasm generated as dasm.js \(hopefully\).
+echo
+
+cd ..
