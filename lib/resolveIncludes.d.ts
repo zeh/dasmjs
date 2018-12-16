92a08b42738275584d55dbfa3c33201e64d074d1
Zeh Fernando <zeh@zehfernando.com>
2018-12-15 20:24:35 -0500

    Recompiled JS output

--- lib/resolveIncludes.d.ts
+++ lib/resolveIncludes.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Resolve all file includes in the source
+ */
 import { IIncludeInfo } from "./index";
 export declare type IGefFileFunc = (entryRelativeUri: string, isBinary: boolean) => string | Uint8Array | undefined;
 export default function resolveIncludes(entrySource: string, getFile?: IGefFileFunc, baseDir?: string, recursive?: boolean): IIncludeInfo[];
